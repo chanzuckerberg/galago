c31bde12d2d284e160eb6045c48e3da0fe50c8ed
Sidney Bell <sidneymbell@gmail.com>
2022-01-10 14:48:27 -0800

    init infrastructure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+ Hello world
+    </div>
+  )
+}
+
+export default App
