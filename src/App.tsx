fa7feb48c17699d2cceeffe01f443e879e742629
Colin Megill <colinmegill@gmail.com>
2022-01-11 13:05:43 -0800

    narrative, clade data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import stub_data from "../stub_data/random_clade_humboldt";
 import Narrative from "./components/narrative";
 import insight_text from "./insight_text/insight_text";
+import stub_data from "../stub_data/random_clade_humboldt";
 
 function App() {
   return (
     <div>
       <h1>Galago</h1>
       <h3>A little tree explorer for public health</h3>
-      <div>
-        for (var insight_type in {insight_text}){" "}
-        {
-          <div>
-            <Narrative
-              key={"cluster_diffusion_if_monophyly_true"}
-              data={stub_data}
-            />
-          </div>
-        }
-        );
-      </div>
+      <Narrative clade={stub_data} />
     </div>
   );
 }
