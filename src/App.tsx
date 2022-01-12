b0ff4340f8f69ddee35fae296f71ed8bdb309ff7
Sidney Bell <sidneymbell@gmail.com>
2022-01-12 12:05:31 -0800

    tada!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import Narrative from "./components/narrative";
-import insight_text from "./insight_text/insight_text";
+import Narrative from "./components/narrative_cluster_diffusion";
 import stub_data from "../stub_data/random_clade_humboldt";
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
     <div>
       <h1>Galago</h1>
       <h3>A little tree explorer for public health</h3>
-      <Narrative clade={stub_data} />
+      <Narrative data={stub_data} />
     </div>
   );
 }
--- src/components/narrative_cluster_diffusion.tsx
+++ src/components/narrative_cluster_diffusion.tsx
@@ -1,35 +1,41 @@
 import { CladeDescription } from "../d";
 
-function ClusterDiffusion(props: CladeDescription) {
-    return (
-      <div>
-        <h3>
-          "What is the smallest genomic cluster that contains all of your
-          samples?"
-        </h3>
-        <h4>
-          "Your {props.n_samples} selected samples are all more closely related
-          to each other than to anything else in this dataset."
-        </h4>
-        <p>
-          This means that the shortest plausible transmission chain connecting
-          your selected samples to each other does not necessarily include any
-          other samples from this dataset.
-        </p>
+type CladeProps = {
+  data: CladeDescription;
+};
 
-)
+function Narrative(props: CladeProps) {
+  const { data } = props;
 
-        <p>
-          These {props.n_samples} samples are separated from each other by
-          {props.min_bn_sample_muts} - {props.max_bn_sample_muts} mutations (or,
-          on average, about {props.min_bn_sample_muts * props.min_trans_per_mut}
-          - {props.max_bn_sample_muts * props.max_trans_per_mut} transmission
-          events.
-        </p>
-        ",
-      </div>
-    );
-  }
+  return (
+    <div
+      style={{ width: `30em`, padding: `10px`, border: `2px solid lightgray` }}
+    >
+      <h5>
+        What is the smallest genomic cluster that contains all of your samples?
+      </h5>
+      <h2>
+        {` 
+        ${
+          data.samples_monophyletic
+            ? `Your ${data.n_samples} selected samples are all more closely related to each other than to anything else in this dataset.`
+            : `Your ${data.n_samples} selected samples are also closely related to samples from ${data.cousin_locations}.`
+        }`}
+      </h2>
+      <p>
+        This means that the shortest plausible transmission chain connecting
+        your selected samples to each other does not necessarily include any
+        other samples from this dataset.
+      </p>
+      <p>
+        {`These ${data.n_samples} samples are separated from each other by
+        ${data.min_bn_sample_muts} - ${data.max_bn_sample_muts} mutations (or,
+        on average, about ${data.min_bn_sample_muts * data.min_trans_per_mut} - 
+        ${data.max_bn_sample_muts * data.max_trans_per_mut} transmission
+        events).`}
+      </p>
+    </div>
+  );
 }
 
-export default ClusterDiffusion;
+export default Narrative;
