72e346a06503eb4cdfcc3a35e616d88383685cbd
Sidney Bell <sidneymbell@gmail.com>
2022-01-12 15:10:01 -0800

    First working draft of cluster definition card

--- src/components/cluster_definition.tsx
+++ src/components/cluster_definition.tsx
@@ -1,9 +1,11 @@
 import { CladeDescription } from "../d";
 
+// THIS KIND OF CARD DESCRIBES A CLADE
 type CladeProps = {
   data: CladeDescription;
 };
 
+// PACKAGE EACH INSIGHT AS ITS OWN REACT COMPONENT SO THAT WE CAN EMBED LOGIC AND DATA WITHIN THE TEXT AND UPDATE IT WHEN THE DATA INPUT CHANGES
 function Narrative(props: CladeProps) {
   const { data } = props;
 
@@ -14,26 +16,43 @@ function Narrative(props: CladeProps) {
         maxWidth: "50em",
       }}
     >
-      <h4>How tightly clustered are your selected samples?</h4>
+      {/* SUBTITLE: WHAT QUESTION ARE WE ANSWERING? */}
+      <h4>How closely related are your selected samples?</h4>
+      {/* TITLE: TAKEHOME / BRIEF ANSWER TO THE QUESTION */}
       <h2>
         {` 
         ${
-          data.samples_monophyletic
-            ? `Your ${data.n_samples} selected samples are all more closely related to each other than to anything else in this dataset.`
-            : `Your ${data.n_samples} selected samples are also closely related to samples from ${data.cousin_locations}.`
+          data.samples_monophyletic // TITLE CHANGES DEPENDING ON IF SAMPLES FORM A MONOPHYLETIC SUBCLADE
+            ? `Your ${data.n_selected_samples} selected samples are all more closely related to each other than to anything else in this dataset.`
+            : `Your ${
+                data.n_selected_samples
+              } selected samples are also closely related to ${
+                data.n_total_samples - data.n_selected_samples
+              } samples from other locations.`
         }`}
       </h2>
+      {/* BODY: SUMMARY OF SUPPORTING DATA AND DEFINITION OF TERMS */}
       <p>
-        This means that the shortest plausible transmission chain connecting
-        your selected samples to each other does not necessarily include any
-        other samples from this dataset.
-      </p>
-      <p>
-        {`These ${data.n_samples} samples are separated from each other by
+        <p>
+          {`Your selected samples are separated from each other by
         ${data.min_bn_sample_muts} - ${data.max_bn_sample_muts} mutations (or,
         on average, about ${data.min_bn_sample_muts * data.min_trans_per_mut} - 
         ${data.max_bn_sample_muts * data.max_trans_per_mut} transmission
         events).`}
+        </p>
+        <p>
+          {` 
+        ${
+          data.samples_monophyletic
+            ? `These samples form their own genomic cluster.`
+            : `The genomic cluster containing your samples also includes ${
+                data.n_total_samples - data.n_selected_samples
+              } samples from these locations: ${
+                data.unselected_sample_locations
+              }.`
+        }
+        Here, "genomic cluster" means the smallest subtree or "clade" that contains all of your samples. You can also think of this as the shortest plausible transmission chain connecting your samples to each other.`}
+        </p>
       </p>
     </div>
   );
--- src/d.ts
+++ src/d.ts
@@ -1,20 +1,25 @@
 export interface CladeDescription {
-  samples: string[];
-  mrca: string;
-  tmrca: number;
-  samples_monophyletic: boolean;
-  cousins: string[];
-  geo_monophyletic: boolean;
-  min_transmissions_across_demes: any;
-  muts_from_parent: number;
-  n_onward_with_accumulated_muts: number;
+  selected_samples: string[];
+  n_selected_samples: number;
 
   min_bn_sample_muts: number;
   max_bn_sample_muts: number;
-  n_samples: number;
-  cousin_locations: string[];
   min_trans_per_mut: number;
   max_trans_per_mut: number;
+
+  mrca: string;
+  tmrca: number;
+  samples_monophyletic: boolean;
+  n_onward_with_accumulated_muts: number;
+  n_total_samples: number;
+
+  unselected_samples: string[];
+  unselected_sample_locations: string[];
+
+  muts_from_parent: number;
+
+  geo_monophyletic: boolean;
+  min_transmissions_across_demes: any;
 }
 
 export interface InsightText {
