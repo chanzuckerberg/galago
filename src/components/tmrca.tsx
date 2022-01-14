4a8de89f07635928347361d2946b7e22f7a36538
Sidney Bell <sidneymbell@gmail.com>
2022-01-14 15:18:02 -0800

    first pass updating tmrca component

--- src/components/tmrca.tsx
+++ src/components/tmrca.tsx
@@ -8,6 +8,11 @@ type CladeProps = {
 // PACKAGE EACH INSIGHT AS ITS OWN REACT COMPONENT SO THAT WE CAN EMBED LOGIC AND DATA WITHIN THE TEXT AND UPDATE IT WHEN THE DATA INPUT CHANGES
 function TMRCA(props: CladeProps) {
   const { data } = props;
+  const mrca_matches = data.selected_samples
+    .filter((s) => s.muts_from_mrca === 0)
+    .concat(
+      data.unselected_samples_in_cluster.filter((s) => s.muts_from_mrca === 0)
+    );
 
   return (
     <div
@@ -22,15 +27,15 @@ function TMRCA(props: CladeProps) {
       {/* TITLE: TAKEHOME / BRIEF ANSWER TO THE QUESTION */}
       <h2>
         {/*TODO: show muts from parent? or shortest path from sample in cluster -> nearest cousin?*/}
-        {`The primary case of this genomic cluster likely existed between ${data.tmrca_min} and ${data.tmrca_max} (95% CI).`}
+        {`The primary case of this genomic cluster likely existed between ${data.mrca.metadata.num_date_confidence[0]} and ${data.mrca.metadata.num_date_confidence[1]} (95% CI).`}
       </h2>
       {/* BODY: SUMMARY OF SUPPORTING DATA AND DEFINITION OF TERMS */}
       <p>
         {`The primary case's pathogen genome sequence 
         ${
-          data.mrca_matches.length == 0
+          mrca_matches.length === 0
             ? "does not match any samples in this dataset."
-            : `was most likely identical to sample(s): ${data.mrca_matches}. Importantly, it is also possible that the true primary case is not be represented in this dataset (but has an identical sequence to these sample(s)). `
+            : `was most likely identical to sample(s): ${mrca_matches}. Importantly, it is also possible that the true primary case is not be represented in this dataset (but has an identical sequence to these sample(s)). `
         }
         `}
       </p>
