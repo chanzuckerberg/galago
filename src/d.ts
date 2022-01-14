b874bd0a3bfe86259ec406a7c606311a7f65a03a
Sidney Bell <sidneymbell@gmail.com>
2022-01-13 19:40:31 -0800

    Overhaul d.ts to match backend output. TODO: update insight components.

--- src/d.ts
+++ src/d.ts
@@ -1,63 +1,50 @@
-export interface CladeDescription {
-  selected_samples: string[];
-  n_selected_samples: number;
-
-  min_bn_sample_muts: number;
-  max_bn_sample_muts: number;
-  min_trans_per_mut: number;
-  max_trans_per_mut: number;
-  // TODO: figure out how to use dates
-  mrca: string;
-  mrca_matches: string[];
-  tmrca: string;
-  tmrca_min: string;
-  tmrca_max: string;
-  samples_monophyletic: boolean;
-  onward_muts_threshold: number;
-  n_onward_with_accumulated_muts: number;
-  n_total_samples: number;
-
-  unselected_samples: string[];
-  unselected_sample_locations: string[];
-
-  muts_from_parent: number;
-  n_cousins: number;
-  cousin_locations: string[];
-  min_cousin_date: string;
-  max_cousin_date: string;
-
-  geo_attr: string;
-  geo_attr_val: string;
-  geo_monophyletic: boolean;
-  min_transmissions_across_demes: number;
-  introductions_source_locations: string[];
+export interface Sample {
+  name: string;
+  location: string;
+  division: string;
+  country: string;
+  region: string;
+  collection_date: string;
+  muts_from_mrca: number;
+  metadata: object;
 }
 
-export interface DatasetDescription {
-  n_samples: number;
-  timepoints: string[];
-  geo_home: {
+export interface Introduction {
+  source_loc: string;
+  date_min: string;
+  date_max: string;
+  n_samples: string;
+}
+
+export interface CladeDescription {
+  selected_samples: Sample[];
+  unselected_samples_in_cluster: Sample[];
+
+  muts_bn_selected: number[];
+  min_trans_per_mut: number;
+  max_trans_per_mut: number;
+
+  // TODO: figure out how to use dates
+  mrca: Sample;
+
+  muts_from_parent: number;
+  cousins: Sample[];
+
+  home_geo: {
     location: string;
     division: string;
     country: string;
     region: string;
   };
-  dataset_census: {
-    location_match: number[];
-    division_match: number[];
-    country_match: number[];
-    region_match: number[];
-    within_2_muts: number[];
-    within_4_muts: number[];
-    within_8_muts: number[];
+
+  transmissions_across_demes: {
+    location: Introduction[];
+    division: Introduction[];
+    country: Introduction[];
+    region: Introduction[];
   };
-  available_census: {
-    location_match: number[];
-    division_match: number[];
-    country_match: number[];
-    region_match: number[];
-    within_2_muts: number[];
-    within_4_muts: number[];
-    within_8_muts: number[];
-  };
+}
+
+export interface DatasetDescription {
+  all_samples: Sample[];
 }
