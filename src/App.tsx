074b909e3cf8852b4b48c755f2bb6e9cc088d807
Sidney Bell <sidneymbell@gmail.com>
2022-01-19 13:48:30 -0800

    comment out everything but tree ingest testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import ClusterDefinition from "./components/cluster_definition";
-import ClusterUniqueness from "./components/cluster_uniqueness";
-import TMRCA from "./components/tmrca";
-import stub_data from "../stub_data/random_clade_humboldt_v2";
-import MinIntroductions from "./components/min_introductions";
-import OnwardTransmission from "./components/onward_transmission";
-import DatasetGeoCompletion from "./components/dataset_geo_completion";
+// import ClusterDefinition from "./components/cluster_definition";
+// import ClusterUniqueness from "./components/cluster_uniqueness";
+// import TMRCA from "./components/tmrca";
+// import stub_data from "../stub_data/random_clade_humboldt_v2";
+// import MinIntroductions from "./components/PAUSED_min_introductions";
+// import OnwardTransmission from "./components/onward_transmission";
+// import DatasetGeoCompletion from "./components/dataset_geo_completion";
+
+import { nextstrain_json } from "../stub_data/ncov_humboldt";
+import { ingest_nextstrain, NSNode } from "./utils/nextstrainAdapter";
 
 function App() {
+  var tree: NSNode = ingest_nextstrain(nextstrain_json);
+  console.log(tree.name);
+
   return (
     <div>
       <h1>Galago</h1>
       <h3>A little tree explorer for public health</h3>
-      <DatasetGeoCompletion data={stub_data.dataset_description} />
+      {/* <DatasetGeoCompletion data={stub_data.dataset_description} />
       <ClusterDefinition data={stub_data.clade_description} />
       <TMRCA data={stub_data.clade_description} />
-      <ClusterUniqueness data={stub_data.clade_description} />
+      <ClusterUniqueness data={stub_data.clade_description} /> */}
       {/* <MinIntroductions data={stub_data.clade_description} /> */}
-      <OnwardTransmission data={stub_data.clade_description} />
+      {/* <OnwardTransmission data={stub_data.clade_description} /> */}
     </div>
   );
 }
