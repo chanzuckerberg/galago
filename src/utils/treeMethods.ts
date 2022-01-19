602b21e6e25b9d4509d4a90be44bb5e339d81d51
Sidney Bell <sidneymbell@gmail.com>
2022-01-18 20:40:10 -0800

    Simplify mrca implementation for readability

--- src/utils/treeMethods.ts
+++ src/utils/treeMethods.ts
@@ -41,60 +41,44 @@ export const find_mrca = (target_nodes: NSNode[]) => {
     return target_nodes[0];
   }
 
-  var ref_node: NSNode | null = null; // first node in the list of targets
-  var ref_node_path: NSNode[] = []; // path between ref_node and the root
+  // iterate over target nodes and record the path between this node and the root of the tree
   var paths = [];
-
   for (var i = 0; i < target_nodes.length; i++) {
-    // iterate over target nodes and record the path between this node and the root of the tree
     var current_node: NSNode | null = target_nodes[i];
-    var this_path = Object();
-    this_path["node"] = current_node;
-    this_path["path"] = [];
+    var this_path = [];
 
     while (current_node) {
-      // while we haven't yet reached the root...
-      this_path["path"].push(current_node);
-      if (!ref_node) {
-        // if no reference node defined yet, add the current_node to the 'ref_node_path' list
-        ref_node_path.push(current_node);
-      }
+      // while we haven't yet reached the root, record each great-great-/great-/grand-/parent
+      this_path.push(current_node);
       if (current_node.parent) {
-        // if not yet at the root, move up to the parent
+        // move up to the parent (the root has no parent)
         current_node = current_node.parent;
       } else {
+        // if we reach the root, break the loop and reset the current_node pointer
         current_node = null;
-      } // if we reach the root, break the loop and reset the current_node pointer
-      current_node = null;
+      }
     }
     paths.push(this_path); // add this path to the list of node paths
-
-    if (!ref_node) {
-      // if no reference node, use the current_node target node
-      ref_node = target_nodes[i];
-    }
   }
 
-  for (i = 0; i < ref_node_path.length; i++) {
-    // the mrca must be contained in the path between the first node and the root. test each one.
-    var current_candidate_node = ref_node_path[i];
+  // the mrca must be contained in every path between each target node and the root. use the first one as a reference to compare to.
+  var reference_path = paths[0];
+  for (i = 0; i < reference_path.length; i++) {
+    var current_candidate_node = reference_path[i];
     var still_a_candidate = true;
-    for (i = 0; i < paths.length; i++) {
-      // iterate through the paths between each target node and the root of the tree
-      var current_target_node = paths[i]["node"];
-      var current_path = paths[i]["path"];
+    for (i = 1; i < paths.length; i++) {
+      // iterate through the paths between each subsequent target node and the root of the tree
+      var current_path = paths[i];
 
-      // if the current candidate node is not present in every path between each target node and the root, move on -- it's not the mrca
-      if (
-        current_target_node !== ref_node &&
-        !current_path.includes(current_candidate_node)
-      ) {
+      // if the current candidate node is not present in any path, it's not the mrca -- move on
+      if (!current_path.includes(current_candidate_node)) {
         still_a_candidate = false;
         break;
       }
 
+      // because nodes are added to each path in order from most recent -> most ancestral, the first time we encounter a node that is in every path, we've found the mrca
       if (still_a_candidate) {
-        var mrca = current_candidate_node; // because nodes are added to each path in order from most recent -> most ancestral, the first time we encounter a node that is in every path, we've found the mrca
+        var mrca = current_candidate_node;
         return mrca;
       }
     }
