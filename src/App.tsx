622a6105e71585c36ba919297b6093c66605e11e
Sidney Bell <sidneymbell@gmail.com>
2022-01-21 17:20:56 -0800

    Re-enable insight components now that the backend works (yayay!)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import ClusterDefinition from "./components/cluster_definition";
-// import ClusterUniqueness from "./components/cluster_uniqueness";
-// import TMRCA from "./components/tmrca";
-// import stub_data from "../stub_data/random_clade_humboldt_v2";
+import ClusterUniqueness from "./components/cluster_uniqueness";
+import TMRCA from "./components/tmrca";
 // import MinIntroductions from "./components/PAUSED_min_introductions";
-// import OnwardTransmission from "./components/onward_transmission";
+import OnwardTransmission from "./components/onward_transmission";
 // import DatasetGeoCompletion from "./components/dataset_geo_completion";
 
 import { nextstrain_json } from "../stub_data/ncov_humboldt";
@@ -11,17 +10,16 @@ import { ingest_nextstrain } from "./utils/nextstrainAdapter";
 import { describe_clade, Node, CladeDescription } from "./d";
 import { random_sample } from "./utils/misc";
 import { get_root, get_leaves } from "./utils/treeMethods";
+import SamplingBias from "./components/sampling-uncertainty";
 
 function App() {
   var tree: Node = ingest_nextstrain(nextstrain_json);
-  console.log("tree root name", tree.name);
 
   var all_samples: Array<Node> = get_leaves(get_root(tree));
   // var dataset: Object = Object.fromEntries(all_samples.map((x) => [x.name, x]));
 
   // var selected_samples: Array<Node> = random_sample(10, all_samples);
-  var selected_samples: Array<Node> = all_samples.slice(-5);
-  console.log("selected...", selected_samples);
+  var selected_samples: Array<Node> = all_samples.slice(-10);
 
   var clade_description: CladeDescription = describe_clade(
     selected_samples,
@@ -39,12 +37,12 @@ function App() {
     <div>
       <h1>Galago</h1>
       <h3>A little tree explorer for public health</h3>
-      {/* <DatasetGeoCompletion data={stub_data.dataset_description} /> */}
+      <SamplingBias data={all_samples} />
       <ClusterDefinition data={clade_description} />
-      {/*      <TMRCA data={stub_data.clade_description} />
-      <ClusterUniqueness data={stub_data.clade_description} /> */}
-      {/* <MinIntroductions data={stub_data.clade_description} /> */}
-      {/* <OnwardTransmission data={stub_data.clade_description} /> */}
+      <ClusterUniqueness data={clade_description} />
+      <TMRCA data={clade_description} />
+      <OnwardTransmission data={clade_description} />
+      {/* <MinIntroductions data={clade_description} /> */}
     </div>
   );
 }
