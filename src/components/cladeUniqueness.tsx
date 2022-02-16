75f5d5a7a81f74b40ca7df10fc909517bb40ed46
Sidney Bell <sidneymbell@gmail.com>
2022-02-15 18:30:09 -0800

    Extensive copy edits

--- src/components/cladeUniqueness.tsx
+++ src/components/cladeUniqueness.tsx
@@ -10,49 +10,87 @@ type CladeProps = {
 function CladeUniqueness(props: CladeProps) {
   const { clade_description } = props;
 
-  const cousin_dates: Array<Date> = clade_description.cousins
-    .map((a) => a.node_attrs.num_date.value)
-    .sort(function (a, b) {
-      const date1 = new Date(a);
-      const date2 = new Date(b);
-      return date1 - date2;
-    });
   const cousin_locations: Array<string> = [
     ...new Set(
       clade_description.cousins.map((a) => a.node_attrs.location.value)
     ),
-  ];
+  ].sort();
 
   const cousin_distances: number[] = clade_description.cousins.map((c: Node) =>
     get_dist([c, clade_description.mrca])
   );
 
+  const local_cousins: Node[] = clade_description.cousins.filter(
+    (c) => c.node_attrs.location.value === clade_description.home_geo.location
+  );
+
+  const local_cousin_dates: Array<Date> = local_cousins
+    .map((a) => a.node_attrs.num_date.value)
+    .sort(function (a, b) {
+      const date1 = new Date(a);
+      const date2 = new Date(b);
+      //@ts-ignore
+      return date1 - date2;
+    });
+
   return (
     <div>
-      <h2>How unique is this clade, relative to the entire dataset? </h2>
+      <h2>
+        How similar or unique is this clade, relative to background community
+        transmission?{" "}
+      </h2>
+      <p style={{ fontStyle: "italic" }}>
+        Comparing the primary case for this clade with its closest relatives in
+        the tree can help us identify whether we have selected the right scope
+        for our investigation.
+      </p>
       <div className="results">
         <p>
-          {/*TODO: show muts from parent? or shortest path from sample in cluster -> nearest cousin?*/}
-          {`This clade is separated by other samples in the dataset by `}
+          The primary case upstream of all the samples in this clade is
+          separated from other samples in the dataset by{" "}
+          <span className="dataPoint">{Math.min(...cousin_distances)}</span> or
+          more mutation(s), or at least{" "}
           <span className="dataPoint">
-            {Math.min(...cousin_distances)}
+            {Math.min(...cousin_distances) *
+              clade_description.muts_per_trans_minmax[0]}{" "}
+            -{" "}
+            {Math.min(...cousin_distances) *
+              clade_description.muts_per_trans_minmax[1]}
           </span>{" "}
-          {`or more mutation(s).`}
+          transmission events.
         </p>
+      </div>
+      <div>
         <p>
-          {`The most closely related "cousins" to your clade of interest include `}
-          <span className="dataPoint">{clade_description.cousins.length}</span>{" "}
-          sample(s) dated between{" "}
-          <span className="dataPoint">
-            {cousin_dates[0].toISOString().substring(0, 10)} and{" "}
-            {cousin_dates[1].toISOString().substring(0, 10)}
-          </span>{" "}
-          {`from these location(s): `}{" "}
-          {cousin_locations.map((l) => (
-            <span className="dataPoint">{l}</span>
-          ))}
-          .
+          If there are many samples that are closely related genetically, have
+          overlapping temporal ranges, and are geographically proximal, you may
+          want to consider expanding the scope of your investigation.{" "}
         </p>
+        <div className="results">
+          <p>
+            In this dataset, the samples most closely related to your clade of
+            interest include{" "}
+            <span className="dataPoint">{local_cousins.length}</span> other
+            samples from {clade_description.home_geo.location}
+            {local_cousins.length < 1
+              ? `.`
+              : `, dated between ${local_cousin_dates[0]
+                  .toISOString()
+                  .substring(0, 10)} and
+              ${local_cousin_dates
+                .slice(-1)[0]
+                .toISOString()
+                .substring(0, 10)}: ${local_cousins.map((s) => s.name)}.`}
+          </p>
+          <p>
+            {clade_description.cousins.length - local_cousins.length < 1
+              ? ``
+              : `There are also ${
+                  clade_description.cousins.length - local_cousins.length
+                } closely related samples from these locations:
+            ${cousin_locations}`}
+          </p>
+        </div>
       </div>
     </div>
   );
