b2ac81dd0ad837cc83a50e075cc3db7ba41729c7
Sidney Bell <sidneymbell@gmail.com>
2022-02-15 19:39:05 -0800

    Copy edits for tmrca

--- src/components/tmrca.tsx
+++ src/components/tmrca.tsx
@@ -2,12 +2,10 @@ import { CladeDescription, Node } from "../d";
 import { get_dist } from "../utils/treeMethods";
 import Sidenote from "./sidenote";
 
-// THIS KIND OF CARD DESCRIBES A CLADE
 type CladeProps = {
   clade_description: CladeDescription;
 };
 
-// PACKAGE EACH INSIGHT AS ITS OWN REACT COMPONENT SO THAT WE CAN EMBED LOGIC AND DATA WITHIN THE TEXT AND UPDATE IT WHEN THE DATA INPUT CHANGES
 function TMRCA(props: CladeProps) {
   const { clade_description } = props;
 
@@ -26,47 +24,63 @@ function TMRCA(props: CladeProps) {
   );
   return (
     <div>
-      <h2>When did this clade arise?</h2>
+      <h2>
+        What was the date and genotype of the primary case upstream of this
+        clade?
+      </h2>
+      <Sidenote
+        num={3}
+        text="You  may also hear this referred to as the 'most recent common ancestor (MRCA)' of a clade."
+      />
+      <p style={{ fontStyle: "italic" }}>
+        We can use the tree to make inferences about the timing and genotype of
+        the primary case <sup style={{ fontSize: "10" }}>3</sup>
+        upstream of all the samples in our clade of interest.
+      </p>
+
       <div className="results">
         <p>
-          {/*TODO: show muts from parent? or shortest path from sample in cluster -> nearest cousin?*/}
-          <>
-            <Sidenote
-              num="5"
-              text={
-                <span>
-                  Learn more about{" "}
-                  <a href="https://alliblk.github.io/genepi-book/fundamental-theory-in-genomic-epidemiology.html#temporally-resolved-phylogenetic-trees.">
-                    inferring dates using phylogenetic trees
-                  </a>{" "}
-                  and{" "}
-                  <a href="https://alliblk.github.io/genepi-book/broad-use-cases-for-genomic-epidemiology.html#estimating-the-start-and-duration-of-an-outbreak.">
-                    how sampling may effect these estimates.
-                  </a>
-                </span>
-              }
-            />
-            This clade's primary case likely occurred{" "}
-            <span className="dataPoint">
-              {clade_description.mrca &&
-              clade_description.mrca.node_attrs.num_date.confidence.length === 2
-                ? `between ${clade_description.mrca.node_attrs.num_date.confidence[0]
-                    .toISOString()
-                    .substring(
-                      0,
-                      10
-                    )} and ${clade_description.mrca.node_attrs.num_date.confidence[1]
-                    .toISOString()
-                    .substring(0, 10)} (95% CI)`
-                : ` around ${clade_description.mrca.node_attrs.num_date.value
-                    .toISOString()
-                    .substring(0, 10)}`}
-            </span>
-            .<sup fontSize="10">5</sup>
-          </>
+          This clade's primary case likely occurred{" "}
+          <span className="dataPoint">
+            {clade_description.mrca &&
+            clade_description.mrca.node_attrs.num_date.confidence.length === 2
+              ? `between ${clade_description.mrca.node_attrs.num_date.confidence[0]
+                  .toISOString()
+                  .substring(
+                    0,
+                    10
+                  )} and ${clade_description.mrca.node_attrs.num_date.confidence[1]
+                  .toISOString()
+                  .substring(0, 10)} (95% CI)`
+              : ` around ${clade_description.mrca.node_attrs.num_date.value
+                  .toISOString()
+                  .substring(0, 10)}`}
+          </span>
         </p>
+      </div>
+      <Sidenote
+        num="4"
+        text={
+          <span>
+            Learn more about{" "}
+            <a href="https://alliblk.github.io/genepi-book/fundamental-theory-in-genomic-epidemiology.html#temporally-resolved-phylogenetic-trees.">
+              inferring dates using phylogenetic trees
+            </a>{" "}
+            and{" "}
+            <a href="https://alliblk.github.io/genepi-book/broad-use-cases-for-genomic-epidemiology.html#estimating-the-start-and-duration-of-an-outbreak.">
+              how sampling may effect these estimates.
+            </a>
+          </span>
+        }
+      />
+      <p>
+        Because pathogens evolve and spread on similar timescales, the number of
+        mutations is proportionate to the amount of time separating two samples
+        <sup style={{ fontSize: "10" }}>4</sup>.
+      </p>
+      <div className="results">
         <p>
-          The primary case's pathogen genome sequence
+          The primary case's pathogen genome sequence{" "}
           {mrca_matches.length === 0 ? (
             "does not match any samples in this dataset."
           ) : (
