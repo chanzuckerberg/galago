4c37057ad7b9739e8f77fc627e57d07351a85788
Sidney Bell <sidneymbell@gmail.com>
2022-03-01 14:02:51 -0800

    Rip out mdx forever

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mdx from "vite-plugin-mdx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx()],
+  plugins: [react()],
 });
