43c50203b11c1e27eb206692a231d78555de08c3
Sidney Bell <sidneymbell@gmail.com>
2022-04-12 12:27:12 -0700

    install thunk

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "./index";
+import thunk from "redux-thunk";
 
-export const store: any = createStore(reducers, {});
+export const store: any = createStore(reducers, {}, applyMiddleware(thunk));
