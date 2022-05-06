ad16d700106763016fa726f00414d615e440d4f3
Sidney Bell <sidneymbell@gmail.com>
2022-05-06 11:20:47 -0700

    Update "about"

--- src/components/landingPage/aboutGalago.tsx
+++ src/components/landingPage/aboutGalago.tsx
@@ -2,16 +2,17 @@ const AboutGalago = () => {
   return (
     <div>
       <h1>Welcome to Galago</h1>
-      <h4>This prototype is still alpha stage -- please pardon our dust!</h4>
+      <h4>
+        Galago is currently an experimental prototype -- please pardon our dust!
+      </h4>
+      <p>
+        Answer basic epidemiological questions using pathogen genomic data,
+        without having to become an expert in phylogenetics.
+      </p>
       <p>
         Developed by genomic epidemiologists, Galago helps public health
         officials interpret phylogenetic trees to make pathogen genomic data
-        more interpretable and actionable during outbreak investigations.
-      </p>
-      <p>
-        Given a tree and a list of samples that you suspect might be part of an
-        outbreak, Galago makes observations about your data and automatically
-        generates a report which answers basic epidemiological questions.
+        relevant and actionable during outbreak investigations.
       </p>
     </div>
   );
