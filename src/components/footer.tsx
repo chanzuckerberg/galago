f371beefa71fe265be0da443529f36cb25e095bf
Sidney Bell <sidneymbell@gmail.com>
2022-05-06 12:33:46 -0700

    FINE @colinmegill

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,12 +11,10 @@ const Footer = () => {
         paddingBottom: "0em",
       }}
     >
-      <p style={{ maxWidth: "65em" }}>
-        Galago: a little tree explorer made with &hearts; by Sidney Bell, PhD &
-        Colin Megill
-        <br /> with many thanks to the CZ Gen Epi team
+      <p style={{ maxWidth: "45em" }}>
+        Galago: a little tree explorer made with &hearts; by the CZ Gen Epi team
       </p>
-      <p style={{ maxWidth: "65em" }}>
+      <p style={{ maxWidth: "45em" }}>
         &copy; 2017 - 2022 CZI &#183; MIT licensed &#183;
         <a href="https://github.com/chanzuckerberg/galago"> Source code</a>
       </p>
