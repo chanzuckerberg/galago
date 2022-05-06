6ca67063aac0f05c37f77644f20fd2f555df0553
Sidney Bell <sidneymbell@gmail.com>
2022-05-06 11:36:25 -0700

    Footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,25 +1,27 @@
 const Footer = () => {
   return (
-    <div>
-      <p
-        style={{
-          //   alignSelf: "center",
-          position: "relative",
-          textAlign: "center",
-          fontSize: ".7em",
-        }}
-      >
-        Methods &#183;{" "}
-        <a href="https://github.com/chanzuckerberg/galago">Code</a> &#183;{" "}
-        <a href="https://github.com/chanzuckerberg/galago/discussions">
-          Discussion
-        </a>
-        &#183; <a href="mailto:sbell@chanzuckerberg.com">Email</a>
-        <br /> Galago: a little tree explorer by Sidney Bell, PhD & Colin Megill
-        <br /> with many thanks to the CZ Gen Epi team
-        <br />
-        &copy; 2017 - 2022 CZI &#183; MIT licensed
-      </p>
+    <div
+      style={{
+        //   alignSelf: "center",
+        position: "relative",
+        textAlign: "center",
+        fontSize: ".7em",
+        maxWidth: "65em",
+        paddingTop: "6em",
+        paddingBottom: "0em",
+      }}
+    >
+      Methods &#183; <a href="https://github.com/chanzuckerberg/galago">Code</a>{" "}
+      &#183;{" "}
+      <a href="https://github.com/chanzuckerberg/galago/discussions">
+        Discussion
+      </a>
+      &#183; <a href="mailto:sbell@chanzuckerberg.com">Email</a>
+      <br /> Galago: a little tree explorer made with &hearts; by Sidney Bell,
+      PhD & Colin Megill
+      <br /> with many thanks to the CZ Gen Epi team
+      <br />
+      &copy; 2017 - 2022 CZI &#183; MIT licensed
     </div>
   );
 };
