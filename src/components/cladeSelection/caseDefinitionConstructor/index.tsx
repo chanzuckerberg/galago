2920733fbc5571f3dbf14bd2e9d13ff8dc3966f6
Sidney Bell <sidneymbell@gmail.com>
2022-06-13 22:12:33 -0700

    Overhaul styling for case definition constructor

--- src/components/cladeSelection/caseDefinitionConstructor/index.tsx
+++ src/components/cladeSelection/caseDefinitionConstructor/index.tsx
@@ -1,12 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import Selectors from "./selectors";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 
 export const caseDefinitionConstructor = () => {
   // @ts-ignore -- TODO: figure out how to add types to state
   const state = useSelector((state) => state.global);
   const dispatch = useDispatch();
-  const [selectedFields, setSelectedFields] = useState<string[]>([]);
+  const [selectedFields, setSelectedFields] = useState<Set<string>>(new Set());
 
   const fieldIsValid = (fieldSummary: any) => {
     if (fieldSummary["dataType"] == "categorical") {
@@ -22,33 +24,44 @@ export const caseDefinitionConstructor = () => {
     }
   };
 
-  return (
-    <div>
-      <h2>Case definition</h2>
-      <label>Choose metadata fields to filter on:</label>
-      <br />
-      <select
-        multiple
-        onChange={(e) => {
-          let selectedFieldsHTMLObject = e.target.selectedOptions;
-          let selectedFields: string[] = [];
-          for (let i = 0; i < selectedFieldsHTMLObject.length; i++) {
-            selectedFields.push(selectedFieldsHTMLObject[i].value);
-          }
-          setSelectedFields(selectedFields);
-        }}
-      >
-        {Object.entries(state.metadataCensus).map((entry, index) => {
-          if (fieldIsValid(entry[1])) {
-            return (
-              <option value={entry[0]} key={index}>
-                {entry[0]}
-              </option>
-            );
-          }
-        })}
-      </select>
+  const validFields: Array<any> = [];
+  Object.entries(state.metadataCensus).forEach((entry, i) => {
+    if (fieldIsValid(entry[1])) {
+      validFields.push({ label: entry[0], id: i });
+    }
+  });
 
+  return (
+    <div style={{ width: "15em", padding: "10px 10px 0px" }}>
+      <h3>Case definition</h3>
+      Optionally, add samples of interest based on a case definition.
+      <Autocomplete
+        multiple
+        style={{ width: "15em", paddingTop: "10px" }}
+        id="tags-outlined"
+        options={validFields}
+        getOptionLabel={(option: any) => option.label}
+        limitTags={5}
+        disableCloseOnSelect
+        size="small"
+        // BUG: set is a temp workaround for the fact that filterSelectedOptions doesn't work such that the same variable can be selected more than once
+        filterSelectedOptions
+        onChange={(event: any, newValue: any) => {
+          let newFields = new Set();
+          newValue.forEach((nv: { label: string; id: number }) =>
+            newFields.add(nv["label"])
+          );
+          //@ts-ignore - all the labels are strings
+          setSelectedFields(newFields); //newValue?.map((nv: any) => nv["label"]));
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Select metadata fields"
+            // placeholder="Favorites"
+          />
+        )}
+      />
       {selectedFields && <Selectors selectedFields={selectedFields} />}
     </div>
   );
--- src/components/cladeSelection/caseDefinitionConstructor/selectors.tsx
+++ src/components/cladeSelection/caseDefinitionConstructor/selectors.tsx
@@ -1,10 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import { caseDefFilter } from "../../../d";
 import Slider from "@mui/material/Slider";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 import ContingencyTable from "../../viz/contingencyTable";
 
 type SelectorsProps = {
-  selectedFields: string[];
+  selectedFields: Set<string>;
 };
 
 export const Selectors = (props: SelectorsProps) => {
@@ -12,92 +16,125 @@ export const Selectors = (props: SelectorsProps) => {
   const state = useSelector((state) => state.global);
   const dispatch = useDispatch();
   const { selectedFields } = props;
+  const selectedFieldsArr = Array.from(selectedFields);
   const metadataCensus = state.metadataCensus;
 
-  const continuousFields = selectedFields.filter(
+  const continuousFields = selectedFieldsArr.filter(
     (field) => metadataCensus[field].dataType === "continuous"
   );
-  const categoricalFields = selectedFields.filter(
+  const categoricalFields = selectedFieldsArr.filter(
     (field) => metadataCensus[field].dataType === "categorical"
   );
 
   return (
-    <>
-      {continuousFields.map((field: string) => {
-        let range = [
-          metadataCensus[field]["min"],
-          metadataCensus[field]["max"],
-        ];
-        return (
-          <p style={{ width: "15em" }}>
-            <label>{field}</label>
-            <Slider
-              size="small"
-              defaultValue={range}
-              //   value={[1, 4]}
-              onChange={(e) => {
-                const newFilter: caseDefFilter = {
-                  field: field,
-                  dataType: "continuous",
-                  min: e.target.value[0],
-                  max: e.target.value[1],
-                };
-                dispatch({
-                  type: "case definition filters updated",
-                  data: newFilter,
-                });
-              }}
-              valueLabelDisplay="auto"
-              step={1}
-              min={range[0]}
-              max={range[1]}
-            />
-          </p>
-        );
-      })}
-
-      {categoricalFields.map((field) => {
-        return (
-          <p>
-            <label>{field}</label>
-            <br />
-            <select
-              multiple
-              onChange={(e) => {
-                let selectedValuesHTMLObject = e.target.selectedOptions;
-                let acceptedValues: string[] = [];
-                for (let i = 0; i < selectedValuesHTMLObject.length; i++) {
-                  acceptedValues.push(selectedValuesHTMLObject[i].value);
-                }
-                const newFilter: caseDefFilter = {
-                  field: field,
-                  dataType: "categorical",
-                  acceptedValues: acceptedValues,
-                };
-                dispatch({
-                  type: "case definition filters updated",
-                  data: newFilter,
-                });
-              }}
-            >
-              {metadataCensus[field]["uniqueValues"].map((val: any) => {
-                return <option value={val.toString()}>{val.toString()}</option>;
-              })}
-            </select>
-          </p>
-        );
-      })}
-      <button
-        onClick={(e) => {
-          dispatch({
-            type: "case definition submitted",
-          });
+    <div>
+      {selectedFields.size > 0 && (
+        <div style={{ width: "15em", paddingTop: "10px" }}>
+          <Button
+            variant="outlined"
+            name="submitCaseDef"
+            onClick={(e) => {
+              dispatch({
+                type: "case definition submitted",
+              });
+            }}
+            size="small"
+            disabled={Object.keys(state.caseDefFilters).length === 0}
+          >
+            Add matches to Samples of Interest
+          </Button>
+        </div>
+      )}
+      <div
+        style={{
+          overflowY: "auto",
+          maxHeight: "12em",
+          overflowX: "hidden",
         }}
       >
-        Submit
-      </button>
+        {continuousFields.map((field: string) => {
+          let range = [
+            metadataCensus[field]["min"],
+            metadataCensus[field]["max"],
+          ];
+          return (
+            <div
+              style={{
+                paddingTop: "10px",
+                paddingLeft: "15px",
+                paddingBottom: "0px",
+              }}
+            >
+              <label>{field}</label>
+              <br />
+              <Slider
+                size="small"
+                style={{ width: "13em" }}
+                defaultValue={range}
+                //   value={[1, 4]}
+                onChange={(e) => {
+                  const newFilter: caseDefFilter = {
+                    field: field,
+                    dataType: "continuous",
+                    min: e.target.value[0],
+                    max: e.target.value[1],
+                  };
+                  dispatch({
+                    type: "case definition filters updated",
+                    data: newFilter,
+                  });
+                }}
+                valueLabelDisplay="auto"
+                step={1}
+                min={range[0]}
+                max={range[1]}
+              />
+            </div>
+          );
+        })}
+
+        {categoricalFields.map((field) => {
+          const allValues = metadataCensus[field]["uniqueValues"];
+          let options: any = [];
+          allValues.forEach((v: string | boolean | number, i: number) => {
+            options.push({ label: v.toString(), id: i });
+          });
+
+          return (
+            <>
+              <Autocomplete
+                multiple
+                style={{ width: "15em", paddingTop: "10px" }}
+                id="tags-outlined"
+                options={options}
+                getOptionLabel={(option: any) => option.label}
+                limitTags={5}
+                disableCloseOnSelect
+                size="small"
+                // BUG: set is a temp workaround for the fact that filterSelectedOptions doesn't work such that the same variable can be selected more than once
+                filterSelectedOptions
+                onChange={(event: any, newValue: any) => {
+                  const acceptedValues = newValue.map((nv: any) => nv.label);
+                  const newFilter: caseDefFilter = {
+                    field: field,
+                    dataType: "categorical",
+                    acceptedValues: [...new Set(acceptedValues)],
+                  };
+                  dispatch({
+                    type: "case definition filters updated",
+                    data: newFilter,
+                  });
+                }}
+                renderInput={(params) => (
+                  <TextField {...params} label={field} />
+                )}
+              />
+            </>
+          );
+        })}
+      </div>
       {/* <ContingencyTable /> */}
-    </>
+    </div>
   );
 };
 
