d218f38324567d831050a8ed05bf0538bcc1681b
Sidney Bell <sidneymbell@gmail.com>
2022-06-14 20:11:58 -0700

    Show full report @ main page

--- src/routes/cladeSelection.tsx
+++ src/routes/cladeSelection.tsx
@@ -5,16 +5,27 @@ import ContactUs from "../components/contactUs";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { SitStat } from "../components/cladeSummary";
-import { get_leaves } from "../utils/treeMethods";
+import { get_leaves, get_root } from "../utils/treeMethods";
 import EpiCurve from "../components/viz/epiCurve";
 import SamplesOfInterest from "../components/cladeSelection/samplesOfInterest";
 import ClusteringOptions from "../components/cladeSelection/clusteringMethodSelect";
 import CaseDefinitionConstructor from "../components/cladeSelection/caseDefinitionConstructor";
 import MutsDateScatter from "../components/viz/mutsDateScatter";
+import Assumptions from "../components/report/assumptions";
+import CladeDefinition from "../components/report/cladeDefinition";
+import CladeUniqueness from "../components/report/cladeUniqueness";
+import GeoSubclades from "../components/report/geoSubclades";
+import OnwardTransmission from "../components/report/onwardTransmission";
+import SamplingBias from "../components/report/sampleDistribTable";
+import TMRCA from "../components/report/tmrca";
+import { gisaidCounts } from "../../data/gisaidCounts2022-06";
+import { useWindowSize } from "@react-hook/window-size";
 
 export default function SampleSelectionRoute() {
   //@ts-ignore
   const state = useSelector((state) => state.global);
+  const [width, height] = useWindowSize();
+
   const allDataPresent =
     state.location && state.division && state.tree && state.loadReport;
   const reportReady = state.cladeDescription && state.tree;
@@ -25,27 +36,45 @@ export default function SampleSelectionRoute() {
       {/* <h1>Select a clade to instantly generate a report</h1> */}
       {/* left side bar */}
       <div style={{ display: "flex" }}>
-        <div>
+        <div
+          style={{
+            width: width / 4,
+            maxWidth: width / 4,
+            overflowX: "hidden",
+            overflowY: "auto",
+          }}
+        >
           <SamplesOfInterest />
           <CaseDefinitionConstructor />
           <ClusteringOptions />
           {/* middle half */}
         </div>
-        <div>
+        <div style={{ width: width / 2 }}>
           <MutsDateScatter />
         </div>
-        <div>
+        <div
+          style={{ overflowY: "auto", width: width / 4, overflowX: "hidden" }}
+        >
           {reportReady ? (
             <>
               <h2>Genomic summary of your selected cluster</h2>
               <SitStat title={false} />
-              <button
-                onClick={(e) => {
-                  navigate("/galago/report");
-                }}
-              >
-                View full report
-              </button>
+              <CladeDefinition sidenote_start={1} />
+              <CladeUniqueness />
+              <TMRCA sidenote_start={3} />
+              <OnwardTransmission sidenote_start={6} />
+              <GeoSubclades />
+              <SamplingBias
+                // @ts-ignore
+                gisaidCounts={gisaidCounts}
+                all_samples={get_leaves(get_root(state.tree))}
+                clade_description={state.cladeDescription}
+                sidenote_start={7}
+              />
+              <Assumptions
+                clade_description={state.cladeDescription}
+                sidenote_start={8}
+              />
             </>
           ) : (
             <p>Select a cluster to instantly generate a report</p>
