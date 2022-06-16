933c5bbcd142b3495b386526969e55a495ce6f16
Sidney Bell <sidneymbell@gmail.com>
2022-06-16 09:31:37 -0700

    Popper + clickaway listener for controls

--- src/components/mainViz.tsx
+++ src/components/mainViz.tsx
@@ -2,8 +2,10 @@ import { useSelector } from "react-redux";
 import { MutsDateScatter } from "./viz/mutsDateScatter";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import { Popover } from "@mui/material";
+import { Card, Popper } from "@mui/material";
 import React, { useState } from "react";
+import SamplesOfInterest from "./cladeSelection/samplesOfInterest";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
 
 export const MainViz = () => {
   // @ts-ignore
@@ -11,10 +13,25 @@ export const MainViz = () => {
   const [viewPlot, setViewPlot] = useState<"scatter" | "tree">(
     state.mrca ? "tree" : "scatter"
   );
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleButtonClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClickAway = () => {
+    setOpen(false);
+  };
+
+  const id = open ? "simple-popper" : undefined;
 
   const chartWidth = 960;
   const chartHeight = 560;
   const chartMargin = 30;
+
+  const controlWidth = 250;
 
   const samplesButtonRef = React.useRef();
 
@@ -26,20 +43,21 @@ export const MainViz = () => {
       }}
     >
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <Button color="secondary" style={{ marginRight: 10 }}>
-          Samples (0)
+        <Button
+          color="secondary"
+          style={{ marginRight: 10 }}
+          onClick={handleButtonClick}
+        >
           Samples of Interest ({state.samplesOfInterest.length})
         </Button>
-        {/* <Popover
-          // id={1233245345}
-          open={true}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-          }}
-        >
-          hello world dddddd
-        </Popover> */}
+        <Popper id={id} open={open} anchorEl={anchorEl}>
+          <ClickAwayListener onClickAway={handleClickAway}>
+            <Card style={{ padding: 15, width: controlWidth }}>
+              <SamplesOfInterest />
+            </Card>
+          </ClickAwayListener>
+        </Popper>
+
         <Button color="secondary" style={{ marginRight: 10 }}>
           Cluster method
         </Button>
