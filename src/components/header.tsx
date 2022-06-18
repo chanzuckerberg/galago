5d7601507fa47f2e5f2d475062fce1c86a2981b0
Sidney Bell <sidneymbell@gmail.com>
2022-06-17 19:45:25 -0700

    Remove x icon on banner

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,39 +9,30 @@ const Header = () => {
 
   return (
     <div>
-      <Collapse in={showAlert}>
-        <Alert
-          severity="info"
-          style={{
-            width: windowWidth * 0.4,
-            position: "absolute",
-            top: 0,
-            right: 0,
-          }}
-          onClose={() => {
-            console.log(
-              "This would close the alert, but we're keeping it open until Galago is 1.0..."
-            );
-            // setShowAlert(false);
-          }}
-        >
-          <AlertTitle>
-            <strong>
-              Galago is still an early-stage prototype - we'd love your
-              feedback!
-            </strong>{" "}
-            <br />
-            You can reach us either{" "}
-            <a href="https://github.com/chanzuckerberg/galago/discussions">
-              on our discussion board
-            </a>{" "}
-            or <a href="mailto:galago@chanzuckerberg.com">via email.</a>
-          </AlertTitle>
-          We're continually working to improve Galago, and plan to release a
-          production version in a few months. In the meantime, please pardon our
-          dust (and don't use Galago for decision making just yet).
-        </Alert>
-      </Collapse>
+      <Alert
+        severity="info"
+        style={{
+          width: windowWidth * 0.4,
+          position: "absolute",
+          top: 0,
+          right: 0,
+        }}
+      >
+        <AlertTitle>
+          <strong>
+            Galago is still an early-stage prototype - we'd love your feedback!
+          </strong>{" "}
+          <br />
+          You can reach us either{" "}
+          <a href="https://github.com/chanzuckerberg/galago/discussions">
+            on our discussion board
+          </a>{" "}
+          or <a href="mailto:galago@chanzuckerberg.com">via email.</a>
+        </AlertTitle>
+        We're continually working to improve Galago, and plan to release a
+        production version in a few months. In the meantime, please pardon our
+        dust (and don't use Galago for decision making just yet).
+      </Alert>
 
       <p
         style={{
