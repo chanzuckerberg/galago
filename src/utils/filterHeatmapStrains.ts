dfe60bb7c5195d04db6673bdb1b159c5ca56b035
Sidney Bell <sidneymbell@gmail.com>
2022-07-22 13:43:32 -0700

    util functions for filtering pairwise distances based on strains of interest

---
+++ src/utils/filterHeatmapStrains.ts
@@ -0,0 +1,48 @@
+import { MutDistances } from "../d";
+
+export const filterToIncludedSamples = (
+  inputData: Array<MutDistances>,
+  strainsToKeep: string[]
+) => {
+  const filteredData = [
+    ...inputData.filter((d: MutDistances) => strainsToKeep.includes(d.strain)),
+  ];
+
+  const filterDistances = (d: MutDistances) => {
+    return {
+      strain: d.strain,
+      distances: d.distances.filter((record) =>
+        strainsToKeep.includes(record.strain)
+      ),
+    };
+  };
+  return filteredData.map(filterDistances);
+};
+
+export const getDefaultSampleSet = (
+  rawData: any,
+  samplesOfInterestNames: string[],
+  maxSamples: number = 50
+) => {
+  // NB: the entries in rawData are already ordered by proximity to the MRCA of the selected clade because of how they're calculated -- i.e., we can just take them in order until full
+  const dataSamplesOfInterest = rawData.filter((datum: any) =>
+    samplesOfInterestNames.includes(datum.strain)
+  );
+  const dataOtherSamples = rawData.filter(
+    (datum: any) => !samplesOfInterestNames.includes(datum.strain)
+  );
+
+  // first add as many samples of interest as we can fit
+  let includedSampleNames: Array<string> = dataSamplesOfInterest
+    .slice(0, 50)
+    .map((d: any) => d.strain);
+
+  // then fill in with other samples, again in order of increasing distance from the MRCA
+  const remainingSpots = maxSamples - includedSampleNames.length;
+  includedSampleNames = includedSampleNames
+    .concat(dataOtherSamples.slice(0, remainingSpots))
+    .map((d: any) => d.strain);
+  console.log("included sample set default", includedSampleNames.length);
+
+  return includedSampleNames;
+};
