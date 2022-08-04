b10df58ed6c9dde59e2481e9986cfdb29a111d27
Sidney Bell <sidneymbell@gmail.com>
2022-08-04 13:41:02 -0700

    Move filter button to clade viz controls; fix alignment

--- src/components/mainViz.tsx
+++ src/components/mainViz.tsx
@@ -12,90 +12,40 @@ import { useDispatch } from "react-redux";
 import { FormControl, FormHelperText } from "@mui/material";
 import { FormControlUnstyled } from "@mui/base";
 import { ForceGraph } from "./viz/drawForceGraph";
+import CladeSlider from "./cladeSelection/cladeSlider";
 
 export const MainViz = () => {
   // @ts-ignore
   const state = useSelector((state) => state.global);
   const dispatch = useDispatch();
   const viewPlot: "scatter" | "forceGraph" = state.viewPlot;
-  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
   const [windowWidth, windowHeight] = useWindowSize();
 
-  const stateCuesHeight = 40;
   const footerHeight = 10;
-  const headerHeight = 100;
-  const extraHeight = headerHeight + footerHeight + stateCuesHeight + 190;
+  const headerHeight = 220;
+  const cladeControlsHeight = 100;
+  const plotToggleHeight = 50;
+  const extraHeight =
+    headerHeight + footerHeight + cladeControlsHeight + plotToggleHeight;
 
-  const chartWidth = windowWidth / 2;
-  const chartHeight = Math.min(
-    ...[(windowWidth / 2) * 0.666, windowHeight - extraHeight]
-  ); // 3:2 aspect ratio
   const chartMargin = 30;
-
-  const toggleDrawer =
-    (anchor: string, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === "keydown" &&
-        ((event as React.KeyboardEvent).key === "Tab" ||
-          (event as React.KeyboardEvent).key === "Shift")
-      ) {
-        return;
-      }
-
-      setDrawerOpen(open);
-      if (open) {
-        dispatch({ type: "view plot toggled", data: "scatter" });
-      }
-    };
+  const chartWidth: number = windowWidth / 2 - chartMargin;
+  const chartHeight = windowHeight - extraHeight;
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
+        justifyContent: "space-between",
+        height: windowHeight - headerHeight - footerHeight,
+        // border: "1px solid yellow",
       }}
     >
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <FormControl>
-          <React.Fragment key={"controlsDrawer"}>
-            <Button
-              variant="outlined"
-              disableElevation
-              disableRipple
-              onClick={toggleDrawer("controlsDrawer", true)}
-            >
-              Filter and suggest clades
-            </Button>
-            <Drawer
-              anchor={"right"}
-              open={drawerOpen}
-              onClose={toggleDrawer("right", false)}
-            >
-              <div style={{ width: windowWidth * 0.4 }}>
-                <ControlsDrawer />
-              </div>
-            </Drawer>
-          </React.Fragment>
-          {/* @ts-ignore */}
-          <FormHelperText size="small">
-            Samples of interest: <u>{state.samplesOfInterestNames.length}</u>
-            <br />
-            Clustering: <u>{state.clusteringMethod}</u>
-            {state.clusteringMethod !== "none" &&
-              state.clusteringMetadataField && (
-                <>
-                  {" "}
-                  on <u>{state.clusteringMetadataField}</u>
-                </>
-              )}
-          </FormHelperText>
-        </FormControl>
+      <div>
+        <CladeSlider chartWidth={chartWidth} />
       </div>
-
-      <div
-        style={{ width: chartWidth, height: chartHeight, margin: chartMargin }}
-      >
+      <div style={{ width: chartWidth, height: chartHeight }}>
         {viewPlot === "forceGraph" && state.mrca ? (
           <ForceGraph
             chartWidth={chartWidth}
@@ -117,7 +67,7 @@ export const MainViz = () => {
           onChange={(e, v) => dispatch({ type: "view plot toggled", data: v })}
           aria-label="viewPlotSelection"
           size="small"
-          style={{ height: 40 }}
+          style={{ height: 30 }}
         >
           <ToggleButton value="scatter" disableRipple>
             Scatterplot of all clades
