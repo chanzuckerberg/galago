eb38adce3273d2fc40911d8e6fe3cc087cd4995e
Sidney Bell <sidneymbell@gmail.com>
2022-08-03 21:18:26 -0700

    tmrca component doesn't mention time if no internal dates

--- src/components/report/tmrca.tsx
+++ src/components/report/tmrca.tsx
@@ -1,5 +1,5 @@
 import { CladeDescription, Node } from "../../d";
-import { get_dist } from "../../utils/treeMethods";
+import { getNodeAttr, get_dist } from "../../utils/treeMethods";
 import Sidenote from "../formatters/sidenote";
 import { FormatDate } from "../formatters/date";
 import { FormatStringArray } from "../formatters/stringArray";
@@ -20,6 +20,10 @@ function TMRCA(props: CladeProps) {
     cladeDescription.unselected_samples_in_cluster
   );
 
+  const haveInternalDates = all_samples.every(
+    (n: Node) => !isNaN(getNodeAttr(n, "num_date"))
+  );
+
   let mrca_distances: { [key: string]: number } = Object.fromEntries(
     all_samples.map((x) => [
       x.name,
@@ -31,19 +35,33 @@ function TMRCA(props: CladeProps) {
   );
   return (
     <div className="reportSection">
-      <h2>What was the date and genotype of the primary infection?</h2>
+      {haveInternalDates ? (
+        <h2>What was the date and genotype of the primary infection?</h2>
+      ) : (
+        <h2>What was the genotype of the primary case?</h2>
+      )}
       <p>
         <Sidenote
           num={sidenote_start}
           text="You may also hear this referred to as the 'most recent common ancestor (MRCA)' of a clade."
         />
       </p>
-      <p style={{ fontStyle: "italic" }}>
-        We can use the tree to make inferences about the timing of the primary
-        case -- and the genotype of the pathogen
-        <sup style={{ fontSize: "10" }}>{sidenote_start}</sup> infecting them --
-        upstream of all the samples in our clade of interest.
-      </p>
+      {haveInternalDates ? (
+        <p style={{ fontStyle: "italic" }}>
+          We can use the tree to make inferences about the timing of the primary
+          case -- and the genotype of the pathogen
+          <sup style={{ fontSize: "10" }}>{sidenote_start}</sup> infecting them
+          -- upstream of all the samples in our clade of interest.
+        </p>
+      ) : (
+        <p style={{ fontStyle: "italic" }}>
+          We can use the tree to make inferences about the genotype of the
+          pathogen
+          <sup style={{ fontSize: "10" }}>{sidenote_start}</sup> infecting the
+          primary case -- upstream of all the samples in our clade of interest.
+        </p>
+      )}
+
       {mrca_matches.length === 0 ? (
         <p className="results">
           The primary case's pathogen genome sequence does not match any samples
@@ -85,54 +103,64 @@ function TMRCA(props: CladeProps) {
           </p>
         </>
       )}
-      <p>
-        <Sidenote
-          num={sidenote_start + 2}
-          text={
-            <span>
-              Learn more about{" "}
-              <a href="https://alliblk.github.io/genepi-book/fundamental-theory-in-genomic-epidemiology.html#temporally-resolved-phylogenetic-trees.">
-                inferring dates using phylogenetic trees
-              </a>{" "}
-              and{" "}
-              <a href="https://alliblk.github.io/genepi-book/broad-use-cases-for-genomic-epidemiology.html#estimating-the-start-and-duration-of-an-outbreak.">
-                how sampling may effect these estimates.
-              </a>
-            </span>
-          }
-        />
-        Even if the true primary case is not represented by a sample in this
-        dataset, because pathogens accumulate mutations at a steady rate over
-        time, we can still infer the approximate date of the primary case
-        <sup style={{ fontSize: "10" }}>{sidenote_start + 2}</sup>.
-      </p>
-      <div className="results">
-        <p>
-          This clade's primary case likely occurred
-          {cladeDescription.mrca &&
-          cladeDescription.mrca.node_attrs.num_date.confidence.length === 2 ? (
-            <>
-              {" "}
-              between{" "}
-              <FormatDate
-                date={cladeDescription.mrca.node_attrs.num_date.confidence[0]}
-              />{" "}
-              and{" "}
-              <FormatDate
-                date={cladeDescription.mrca.node_attrs.num_date.confidence[1]}
-              />
-              (95% CI)
-            </>
-          ) : (
-            <>
-              around{" "}
-              <FormatDate
-                date={cladeDescription.mrca.node_attrs.num_date.value}
-              />
-            </>
-          )}
-        </p>
-      </div>
+      {haveInternalDates && (
+        <>
+          <p>
+            <Sidenote
+              num={sidenote_start + 2}
+              text={
+                <span>
+                  Learn more about{" "}
+                  <a href="https://alliblk.github.io/genepi-book/fundamental-theory-in-genomic-epidemiology.html#temporally-resolved-phylogenetic-trees.">
+                    inferring dates using phylogenetic trees
+                  </a>{" "}
+                  and{" "}
+                  <a href="https://alliblk.github.io/genepi-book/broad-use-cases-for-genomic-epidemiology.html#estimating-the-start-and-duration-of-an-outbreak.">
+                    how sampling may effect these estimates.
+                  </a>
+                </span>
+              }
+            />
+            Even if the true primary case is not represented by a sample in this
+            dataset, because pathogens accumulate mutations at a steady rate
+            over time, we can still infer the approximate date of the primary
+            case
+            <sup style={{ fontSize: "10" }}>{sidenote_start + 2}</sup>.
+          </p>
+          <span className="results">
+            <p>
+              This clade's primary case likely occurred
+              {cladeDescription.mrca &&
+              cladeDescription.mrca.node_attrs.num_date.confidence.length ===
+                2 ? (
+                <>
+                  {" "}
+                  between{" "}
+                  <FormatDate
+                    date={
+                      cladeDescription.mrca.node_attrs.num_date.confidence[0]
+                    }
+                  />{" "}
+                  and{" "}
+                  <FormatDate
+                    date={
+                      cladeDescription.mrca.node_attrs.num_date.confidence[1]
+                    }
+                  />
+                  (95% CI)
+                </>
+              ) : (
+                <>
+                  around{" "}
+                  <FormatDate
+                    date={cladeDescription.mrca.node_attrs.num_date.value}
+                  />
+                </>
+              )}
+            </p>
+          </span>
+        </>
+      )}
     </div>
   );
 }
