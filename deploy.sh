f4a1af123bbc29ffa172fdb9d2e671b9884f3b64
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-08-18 14:43:27 -0700

    Troubleshoot deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ git checkout main
 npm run build
 git checkout gh-pages
 git rm -rf .
-mv dist/* . && rmdir dist/ 
+mv dist/* . && rmdir dist/
 git add index.html assets/
-git commit -m "deploy" --no-verify
+git commit -m "deploy old school" --no-verify
 git push
 git checkout main
