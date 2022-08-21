0220434e1c77ae7d2aa5706cba069bf6cf0cb02a
Sidney Bell <sidneymbell@gmail.com>
2022-08-20 21:21:58 -0700

    Minor txt tweaks

--- src/components/landingPage/demo.tsx
+++ src/components/landingPage/demo.tsx
@@ -31,11 +31,13 @@ export const Demo = (props: DemoProps) => {
         Genomic epidemiology helped public health officials understand a
         real-world outbreak of SARS-CoV-2 at a farm in Humboldt County. This
         demo report is automatically generated based on the same phylogenetic
-        tree that was used in this investigation.{" "}
+        tree that was used in this investigation. <br />
         <a href="https://www.medrxiv.org/content/10.1101/2021.09.21.21258385v1">
-          You can read more about this outbreak here.
+          Read more about this outbreak
         </a>
+        .
       </p>
+      <p style={{ marginTop: 40, height: 37 }} />
       <p style={{ width: 200 }}>
         <Button
           variant="contained"
