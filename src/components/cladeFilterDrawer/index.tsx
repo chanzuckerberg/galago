5ba8626bfed23085d3689569b6b1bec285dd7112
Sidney Bell <sidneymbell@gmail.com>
2022-08-31 20:30:28 -0700

    FAB -> button

--- src/components/cladeFilterDrawer/index.tsx
+++ src/components/cladeFilterDrawer/index.tsx
@@ -1,13 +1,11 @@
 import SamplesOfInterest from "./samplesOfInterest";
-import CaseDefinitionConstructor from "./caseDefinitionConstructor";
 import ClusteringOptions from "./clusteringMethodSelect";
 import { useWindowSize } from "@react-hook/window-size";
 import Divider from "@mui/material/Divider";
-import Fab from "@mui/material/Fab";
-import ClearIcon from "@mui/icons-material/Clear";
 import Tooltip from "@mui/material/Tooltip";
 import { useDispatch } from "react-redux";
 import { tooltipProps } from "../formatters/sidenote";
+import Button from "@mui/material/Button";
 
 export const CladeFilterDrawer = () => {
   const [windowWidth, windowHeight] = useWindowSize();
@@ -16,59 +14,71 @@ export const CladeFilterDrawer = () => {
   return (
     <div
       style={{
-        width: windowWidth * 0.4,
+        width: windowWidth * 0.33,
         margin: "auto",
         marginTop: 50,
         paddingBottom: 100,
+        marginRight: windowWidth * 0.085,
       }}
     >
       <div>
         <h1>Filter clades (preview on the left)</h1>
         <ClusteringOptions />
         <Divider variant="middle" style={{ margin: 30 }} />
+        <SamplesOfInterest />
 
         <div
           style={{
             position: "fixed",
-            bottom: 10,
-            right: 50,
-            display: "flex",
-            flexDirection: "row",
-            width: 130,
-            justifyContent: "space-between",
+            bottom: 0,
+            paddingTop: 10,
+            paddingBottom: 10,
+            paddingLeft: windowWidth * 0.085,
+            left: windowWidth * 0.5,
+            height: 40,
+            width: windowWidth * 0.5,
+            backgroundColor: "white",
+            borderTop: "1px solid lightGray",
           }}
         >
-          <Tooltip
-            title="Apply new filters (previewed to the left)"
-            componentsProps={tooltipProps}
-            arrow
-          >
-            <Fab
-              variant="extended"
-              size="large"
-              color="primary"
-              aria-label="apply"
-              onClick={() => {
-                dispatch({ type: "filter drawer changes applied" });
-              }}
+          <div>
+            <Tooltip
+              title="Apply new filters (previewed to the left)"
+              componentsProps={tooltipProps}
+              arrow
             >
-              Apply
-            </Fab>
-          </Tooltip>
-          <Tooltip title="Discard changes" componentsProps={tooltipProps} arrow>
-            <Fab
-              size="small"
-              color="secondary"
-              aria-label="cancel"
-              onClick={() => {
-                dispatch({ type: "filter drawer changes cancelled" });
-              }}
+              <Button
+                variant="contained"
+                size="large"
+                color="primary"
+                aria-label="apply"
+                onClick={() => {
+                  dispatch({ type: "filter drawer changes applied" });
+                }}
+                sx={{ paddingLeft: 5, paddingRight: 5, marginRight: 3 }}
+              >
+                Apply
+              </Button>
+            </Tooltip>
+            <Tooltip
+              title="Discard changes"
+              componentsProps={tooltipProps}
+              arrow
             >
-              <ClearIcon />
-            </Fab>
-          </Tooltip>
+              <Button
+                variant="text"
+                size="large"
+                color="primary"
+                aria-label="cancel"
+                onClick={() => {
+                  dispatch({ type: "filter drawer changes cancelled" });
+                }}
+              >
+                CANCEL
+              </Button>
+            </Tooltip>
+          </div>
         </div>
-        <SamplesOfInterest />
       </div>
     </div>
   );
