b58d156cc69d6abdde77084950f061b7c4d776c3
Sidney Bell <sidneymbell@gmail.com>
2022-10-20 10:52:44 -0700

    Accordion main report sections

--- src/components/report/assumptions.tsx
+++ src/components/report/assumptions.tsx
@@ -1,6 +1,12 @@
 import Sidenote from "../formatters/sidenote";
-import { CladeDescription } from "../../d";
 import { useSelector } from "react-redux";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import { FormatDataPoint } from "../formatters/dataPoint";
+import { ROUTES } from "src/routes";
+import { Link } from "react-router-dom";
 
 type AssumptionsProps = {
   sidenote_start: number;
@@ -14,43 +20,43 @@ function Assumptions(props: AssumptionsProps) {
   return (
     <div className="reportSection">
       <h2> What assumptions influence this report?</h2>
-      <p style={{ fontStyle: "italic" }}>
-        Most of the data and insights described in this report are strictly
-        observational (meaning they don't depend on heuristics or "judgement
-        calls"). However, there are a few exceptions:
-      </p>
-      <span className="results">
-        <ul>
-          <li>
-            We assume that the{" "}
+      <Accordion elevation={1} disableGutters square>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          sx={{ marginBottom: 0, paddingBottom: 0 }}
+        >
+          <p className="resultsSummary">
+            The data and insights described in this report are strictly
+            observational (meaning they don't depend on heuristics or "judgement
+            calls"). The most important parameter is the average number of
+            mutations per transmission:{" "}
+            <FormatDataPoint value={`0 - ${state.mutsPerTransmissionMax}`} />
+          </p>
+        </AccordionSummary>
+        <AccordionDetails>
+          <p className="theory">
+            We{" "}
             <Sidenote
-              target={`number of mutations`}
-              contents={`This determines which samples are mostly likely from tertiary cases
-      (onward transmission).`}
+              target={`estimate`}
+              contents={
+                <Link
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  style={{ color: "white" }}
+                  to={ROUTES.METHODS}
+                >
+                  Methods
+                </Link>
+              }
             />{" "}
-            that occurs with each transmission ranges between{" "}
-            <span className="dataPoint">
-              0 - {state.mutsPerTransmissionMax}
-            </span>
-          </li>
-          <br />
-          <li>
-            When looking for samples that are outside your clade by closely
-            related ("cousins"), we trace back to the most recent grand/parent
-            that is{" "}
-            <Sidenote
-              target={"separated from the primary case by"}
-              contents={`
-Higher values mean that we search more broadly for "cousins".`}
-            />{" "}
-            at least{" "}
-            <span className="dataPoint">
-              {cladeDescription.min_muts_to_parent}
-            </span>{" "}
-            mutation(s).
-          </li>
-        </ul>
-      </span>
+            the number of mutations per transmission based on the pathogen
+            mutation rate, serial interval, and the poisson probability of
+            observing more than N mutations between directly linked samples.
+            This determines which samples are mostly likely from tertiary cases
+            (onward transmission).
+          </p>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
--- src/components/report/geoSubclades.tsx
+++ src/components/report/geoSubclades.tsx
@@ -2,10 +2,15 @@ import { CladeDescription } from "../../d";
 import { FormatDataPoint } from "../formatters/dataPoint";
 import { FormatStringArray } from "../formatters/stringArray";
 import { useSelector } from "react-redux";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function GeoSublades() {
   //@ts-ignore
   const state = useSelector((state) => state.global);
+
   const cladeDescription = state.cladeDescription;
 
   const no_data =
@@ -33,53 +38,83 @@ function GeoSublades() {
         Has transmission between geographic areas contributed to this clade?
       </h2>
 
-      <p style={{ fontStyle: "italic" }}>
-        By overlaying the geographic location of samples with the hierarchical
-        clustering from the tree, we can infer the minimum number of
-        transmissions between locations that have contributed to your clade of
-        interest.
-      </p>
+      <Accordion elevation={1} disableGutters square>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          sx={{ marginBottom: 0, paddingBottom: 0 }}
+        >
+          {geo_monophyletic ? (
+            <p className="resultsSummary">
+              All of the samples in this clade are from the same{" "}
+              <FormatDataPoint value={cladeDescription.subclade_geo} />
+              -- it is plausible that these samples were all the result of a
+              single introduction, followed by local spread.
+            </p>
+          ) : (
+            <p className="resultsSummary">
+              At least{" "}
+              <FormatDataPoint value={cladeDescription.subclades.length} />{" "}
+              separate introductions to
+              <FormatDataPoint
+                value={state[cladeDescription.subclade_geo]}
+              />{" "}
+              have contributed to this clade.
+            </p>
+          )}
+        </AccordionSummary>
 
-      <p className="results">
-        {/* {no_data &&
+        <AccordionDetails>
+          <p className="theory">
+            By overlaying the geographic location of samples with the
+            hierarchical clustering from the tree, we can infer the minimum
+            number of transmissions between locations that have contributed to
+            your clade of interest.
+          </p>
+
+          <p className="results">
+            {/* {no_data &&
           "While there are samples within your clade from other locations, we aren't able to report on specific transmissions between locations because this was not inferred by Nextstrain upstream."} */}
 
-        {!no_data && geo_monophyletic && (
-          <>
-            All of the samples in this clade are from the same{" "}
-            <FormatDataPoint value={cladeDescription.subclade_geo} />; we do not
-            see evidence of multiple introductions to{" "}
-            <FormatDataPoint
-              //@ts-ignore
-              value={cladeDescription.home_geo[cladeDescription.subclade_geo]}
-            />
-            <br />
-            <br />
-            Thus, it is plausible that these samples were all the result of a
-            single introduction, followed by local spread.
-          </>
-        )}
+            {!no_data && geo_monophyletic && (
+              <>
+                All of the samples in this clade are from the same{" "}
+                <FormatDataPoint value={cladeDescription.subclade_geo} />; we do
+                not see evidence of multiple introductions to{" "}
+                <FormatDataPoint
+                  //@ts-ignore
+                  value={
+                    cladeDescription.home_geo[cladeDescription.subclade_geo]
+                  }
+                />
+                <br />
+                <br />
+                Thus, it is plausible that these samples were all the result of
+                a single introduction, followed by local spread.
+              </>
+            )}
 
-        {!no_data && !geo_monophyletic && (
-          <>
-            At least{" "}
-            <FormatDataPoint value={cladeDescription.subclades.length} />{" "}
-            transmission(s) between{" "}
-            <FormatDataPoint
-              value={
-                //@ts-ignore
-                cladeDescription["home_geo"][cladeDescription.subclade_geo]
-              }
-            />{" "}
-            and these locations have contributed to this clade:{" "}
-            <FormatStringArray values={subclade_locations} />
-          </>
-        )}
-      </p>
-      <p>
-        As always, the strength of this evidence depends on how representative
-        your dataset is (see below).
-      </p>
+            {!no_data && !geo_monophyletic && (
+              <>
+                At least{" "}
+                <FormatDataPoint value={cladeDescription.subclades.length} />{" "}
+                transmission(s) between{" "}
+                <FormatDataPoint
+                  value={
+                    //@ts-ignore
+                    cladeDescription["home_geo"][cladeDescription.subclade_geo]
+                  }
+                />{" "}
+                and these locations have contributed to this clade:{" "}
+                <FormatStringArray values={subclade_locations} />
+              </>
+            )}
+          </p>
+          <p>
+            As always, the strength of this evidence depends on how
+            representative your dataset is (see below).
+          </p>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
