0b198c27ee88bf401fd2db3aa19e9a1ff3a80c23
Sidney Bell <sbell@chanzuckerberg.com>
2024-11-01 13:51:21 -0700

    Update main page footer

--- src/components/bigFoot.tsx
+++ src/components/bigFoot.tsx
@@ -4,88 +4,69 @@ import { ROUTES } from "src/routes";
 
 const BigFoot = () => {
   return (
-    <div
+    <div // entire footer
       style={{
         position: "relative",
         bottom: 0,
         left: 0,
         backgroundColor: "#333",
         color: "#FFF",
-        height: 50,
+        // height: 50,
         display: "flex",
-        flexDirection: "row",
+        flexDirection: "column",
         justifyContent: "space-between",
-        padding: 20,
+        padding: "5px 15px 20px 20px",
         margin: 0,
-        borderRadius: 5,
+        borderTopLeftRadius: "5px",
+        borderTopRightRadius: "5px",
       }}
     >
-      <div // content on the left
+      <div // top row
         style={{
           display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "baseline",
         }}
       >
-        <div // first row on left
+        <div // top left
           style={{
             display: "flex",
-            flexDirection: "row",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "baseline",
           }}
         >
-          <img
-            src="https://github.com/chanzuckerberg/galago/raw/main/src/images/czge-logo.svg"
-            style={{ width: 20, marginRight: 10 }}
-          />
-          <Divider
-            orientation="vertical"
-            variant="middle"
-            flexItem
-            sx={{
-              bgcolor: "secondary.light",
-              height: 20,
-              position: "relative",
-              top: 15,
+          <span
+            style={{
+              color: "white",
+              fontFamily: "Noto Sans",
+              fontWeight: "bold",
+              fontSize: 20,
+              paddingRight: 3,
             }}
-          />
-
-          <h2 style={{ color: "white", marginLeft: 10 }}>Galago</h2>
+          >
+            Galago
+          </span>
         </div>
-        <div // second row on left
-          style={{
-            color: "white",
-            fontSize: 12,
-            width: 400,
-            position: "relative",
-            top: -10,
-          }}
-        >
-          a little tree explorer made with &hearts; by the CZ GEN EPI team
-        </div>
-      </div>
-      <div // content on the right
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
-          color: "white",
-        }}
-      >
-        <div // first row on the right
+        <div // top right
           style={{
             display: "flex",
             flexDirection: "row",
-            justifyContent: "space-evenly",
-            width: 700,
-            maxWidth: 700,
-            position: "relative",
-            top: 12,
+            justifyContent: "right",
+            width: 300,
+            height: 35,
+            alignItems: "flex-end",
           }}
         >
           <Link
             target="_blank"
             rel="noreferrer noopener"
-            style={{ color: "white", textDecoration: "none" }}
+            style={{
+              color: "white",
+              textDecoration: "none",
+              padding: "0px 12px",
+            }}
             to={ROUTES.FAQ}
           >
             FAQs
@@ -93,7 +74,11 @@ const BigFoot = () => {
           <Link
             target="_blank"
             rel="noreferrer noopener"
-            style={{ color: "white", textDecoration: "none" }}
+            style={{
+              color: "white",
+              textDecoration: "none",
+              padding: "0px 12px",
+            }}
             to={ROUTES.METHODS}
           >
             Methods
@@ -101,54 +86,51 @@ const BigFoot = () => {
           <a
             target="_blank"
             rel="noreferrer noopener"
-            href="https://help.czgenepi.org/hc/en-us/categories/6217716150804-Genomic-Epidemiology-Learning-Center"
-            style={{ color: "white", textDecoration: "none" }}
+            href="mailto:sidneymbell@gmail.com"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              padding: "0px 12px",
+            }}
           >
-            Learning Center
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            href="https://github.com/chanzuckerberg/galago/discussions"
-            style={{ color: "white", textDecoration: "none" }}
-          >
-            Discussion Board
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            href="mailto:galago@chanzuckerberg.com"
-            style={{ color: "white", textDecoration: "none" }}
-          >
-            Contact Us
+            Contact
           </a>
           <a
             target="_blank"
             rel="noreferrer noopener"
             href="https://github.com/chanzuckerberg/galago"
-            style={{ color: "white", textDecoration: "none" }}
+            style={{ color: "white", textDecoration: "none", paddingLeft: 12 }}
           >
             Github
           </a>
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            href="https://czgenepi.org/"
-          >
-            <img
-              src="https://github.com/chanzuckerberg/czgenepi/raw/trunk/src/frontend/src/common/images/logo_complete_white.svg"
-              style={{ width: 100 }}
-            />
-          </a>
         </div>
-        <div
+      </div>
+      <div //bottom left
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "baseline",
+        }}
+      >
+        <div>
+          <span
+            style={{
+              color: "white",
+              fontSize: 12,
+              width: 400,
+            }}
+          >
+            a little tree explorer made with &hearts; by Sidney Bell & Colin
+            Megill
+          </span>
+        </div>
+        <div // bottom right
           style={{
             fontSize: 12,
             display: "flex",
             flexDirection: "row",
             justifyContent: "right",
-            paddingRight: 16,
-            paddingTop: 20,
           }}
         >
           License: MIT (code) & CC0 (content)
